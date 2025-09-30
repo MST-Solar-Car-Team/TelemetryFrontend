857718fb101f91d725488f85cc6a763f086aa777
Anthony Nguyen <anvzm@umsystem.edu>
2025-09-29 20:21:52 -0500

    I made a box

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Shitty Login page</h1>
+  <form>
+    Garbo Data Server login <br>
+    <label for="username">Username</label>
+    <input
+      type="text"
+      id="usernameBox"
+      name="username"
+      required
+      minlength="4"
+      maxlength="8"
+      size="10" /> <br>
+    <label for="pwd">Password</label>
+    <input
+      type="password"
+      id="pwd"
+      name="pwd"
+      required
+      minlength="4"
+      maxlength="8"
+      size="10" />
+  </form>
+  <img id="devLogo" src= "./images/devArt.png" alt="100% the Solar Car logo" width="10%" class="center">
 </template>
 
-<style scoped></style>
+<style scoped>    
+  input {
+  width: 15%;
+  border: 1px solid green;
+  padding: 1px;
+  margin: 5px;
+  text-align: center;
+}
+ .center {
+  display: block;
+  margin-top: 10%;
+  margin-left: auto;
+  margin-right: auto;
+  }</style>
+
+<script>
+  export default {}
+</script>
